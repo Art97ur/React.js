ede29b33b2245e8e11c9c073027a07c7bf68f516
Armen Marjinyan <armenmarjinyan@MacBook-Pro-Armen.local>
2022-07-20 19:48:04 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import './App.css'
+class App extends React.Component {
+  state = {
+    users: []
+  }
+  componentDidMount() {
+    fetch("https://randomuser.me/api?results=20&inc=name,picture,dob")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ users: data.results })
+      })
+  }
+  remove(index) {
+    this.setState(curr => {
+      curr.users.splice(index, 1)
+      return curr
+    })
+  }
+  render() {
+    const { users } = this.state
+    return <div>
+      <h3>Now we have {users.length} users</h3>
+      <div id="users">
+        {
+          users.map((elm, index) => {
+            return <div>
+              <img src={elm.picture.large} />
+              <h5>{elm.name.first} {elm.name.last}</h5>
+              <p>{elm.dob.age} years old!</p>
+              <button onClick={() => this.remove(index)}>Delete</button>
+            </div>
+          })
+        }
+      </div>
+    </div >
+  }
 }
-
-export default App;
+export default App
